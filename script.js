d1ac142ca138d59f4165b7b07e5e84441dc795de
Oldee18 <oldee18@gmail.com>
2025-01-05 15:26:05 +0100

    Created the human choice prompt, and declared 2 new variables for the scores

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 let choices = ("rock", "paper", "scissors");
+let humanScore = 0;
+let computerScore = 0;
 
     function getComputerChoice() {
         
         let computerChoice = Math.random();
             if (computerChoice <= 0.33) {
-                console.log("The computer choose " + "rock".toUpperCase());
+                console.log("The computer chose " + "rock".toUpperCase());
             } else if (computerChoice > 0.33 && computerChoice <= 0.66){
-                console.log("The computer choose " + "paper".toUpperCase());
+                console.log("The computer chose " + "paper".toUpperCase());
             } else if (computerChoice > 0.66) 
-                console.log("The computer choose " + "scissors".toUpperCase());
+                console.log("The computer chose " + "scissors".toUpperCase());
             }
-        
-        
 
+
+
+    function getHumanChoice() {
+
+        let humanChoice = prompt("What is your choice?").toLocaleLowerCase();
+            if (humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors") {
+                console.log("You chose " + humanChoice);
+            } else {
+                console.log("This choice was not part of the game!!! Choice another!")
+            }
+    }
+
+getHumanChoice()
 getComputerChoice()

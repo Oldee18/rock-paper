b3d9166b87c036bf1f65c5beb2dc4d0a4fdbbeb9
Oldee18 <oldee18@gmail.com>
2025-01-05 15:06:25 +0100

    Added the computer choice, and randomized it

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello World")
+let choices = ("rock", "paper", "scissors");
+
+    function getComputerChoice() {
+        
+        let computerChoice = Math.random();
+            if (computerChoice <= 0.33) {
+                console.log("The computer choose " + "rock".toUpperCase());
+            } else if (computerChoice > 0.33 && computerChoice <= 0.66){
+                console.log("The computer choose " + "paper".toUpperCase());
+            } else if (computerChoice > 0.66) 
+                console.log("The computer choose " + "scissors".toUpperCase());
+            }
+        
+        
+
+getComputerChoice()

ffd90f4b5042401aae54bfcb3d1e456235037618
Oldee18 <oldee18@gmail.com>
2025-01-14 19:26:04 +0100

    Added the buttons on html, and eventlistener on js, need to repair the logic for the winner

--- script.js
+++ script.js
@@ -18,6 +18,44 @@ const tie = "It's a tie! Try again!"
 let humanScore = 0;
 let computerScore = 0;
 
+
+let humanChoice = ""
+  
+    const getHumanChoice = function () {
+
+        const rockBtn = document.querySelector("#rock")
+        const paperBtn = document.querySelector("#paper")
+        const scissorsBtn = document.querySelector("#scissors")
+        
+        rockBtn.addEventListener("click", () => {
+            humanChoice = choices[0]
+            console.log(choices[0])
+            playGame()
+        })
+        paperBtn.addEventListener("click", () => {
+            humanChoice = choices[1]
+            console.log(choices[1])
+            playGame()
+        })
+        scissorsBtn.addEventListener("click", () => {
+            humanChoice = choices[2]
+            console.log(choices[2])
+            playGame()
+            })
+        /* let humanChoice = prompt("Do you choose rock, paper or scissors?").toLocaleLowerCase();
+            if (humanChoice === "rock") {
+                humanChoice = choices[0]
+                console.log(choices[0])
+            } else if (humanChoice === "paper") {
+                humanChoice = choices[1]
+                console.log(choices[1])
+            } else if (humanChoice === "scissors") {
+                humanChoice = choices[2]
+                console.log(choices[2]);
+            }   */
+        return humanChoice;
+    }
+
 const playGame = function () {
 
     const getComputerChoice = function () {
@@ -34,25 +72,11 @@ const playGame = function () {
             }
         return computerChoice;
     }
-    const getHumanChoice = function () {
-        let humanChoice = prompt("Do you choose rock, paper or scissors?").toLocaleLowerCase();
-            if (humanChoice === "rock") {
-                humanChoice = choices[0]
-                console.log(choices[0])
-            } else if (humanChoice === "paper") {
-                humanChoice = choices[1]
-                console.log(choices[1])
-            } else if (humanChoice === "scissors") {
-                humanChoice = choices[2]
-                console.log(choices[2]);
-            }   
-        return humanChoice;
-    }
 
-    const humanSelection = getHumanChoice()
+    const humanSelection = humanChoice
     const computerSelection = getComputerChoice()
 
-    const playRound = function () {
+    
         if (humanSelection === computerSelection) {
             console.log(tie,)
         } else if (humanSelection > computerSelection) {
@@ -62,17 +86,20 @@ const playGame = function () {
             humanScore++
             console.log(humanWin,)
         }
-    }
+    
 
-
-    playRound()
     let scores = `You have ${humanScore} score, and the computer have ${computerScore}`
     console.log(scores)
+   
+  
 }
 
-const playFiveGame = function () {
+getHumanChoice()
+
+
+/*const playFiveGame = function () {
     for (let i = 1; i<6; i++ ) {
         playGame()
     }
 }
-playFiveGame()
+playFiveGame() */

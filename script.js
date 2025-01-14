6d5ada569323e3e006aae9461e1671851397252a
Oldee <oldee18@gmail.com>
2025-01-14 14:36:39 +0100

    Added a function for playing a round

--- script.js
+++ script.js
@@ -4,33 +4,57 @@ const Choice = {
     SCISSORS: "SCISSORS"
     }
 
-const choices = [
+let choices = [
     Choice.ROCK,
     Choice.PAPER,
     Choice.SCISSORS
-    ]    
+    ]
 
+const humanWin = "You WIN!"
+const computerWin = "The computer win. Try again!"
+const tie = "It's a tie! Try again!"
 
 let humanScore = 0;
 let computerScore = 0;
 
 const getComputerChoice = function () {
-    const computerChoice = Math.floor(Math.random()*3)
+    let computerChoice = Math.floor(Math.random()*3)
         if (computerChoice == 0){
-            return console.log(choices[0])
+            computerChoice = choices[0]
+            console.log(choices[0])
         } else if (computerChoice == 1) {
-            return console.log(choices[1])
-        } else 
-        return console.log(choices[2])
+            computerChoice = choices[1]
+            console.log(choices[1])
+        } else if (computerChoice == 2) {
+            computerChoice = choices[2]
+            console.log(choices[2]);
+        }    
+    return computerChoice;
 }
 const getHumanChoice = function () {
-    const humanChoice = prompt("Do you choose rock, paper or scissors?").toLocaleLowerCase();
+    let humanChoice = prompt("Do you choose rock, paper or scissors?").toLocaleLowerCase();
         if (humanChoice === "rock") {
+            humanChoice = choices[0]
             console.log(choices[0])
         } else if (humanChoice === "paper") {
+            humanChoice = choices[1]
             console.log(choices[1])
-        } else 
-            console.log(choices[2])
+        } else if (humanChoice === "scissors") {
+            humanChoice = choices[2]
+            console.log(choices[2]);
+        }    
+    return humanChoice;
+}
+
+const playRound = function (humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log(tie)
+    } else if (humanChoice > computerChoice) {
+        console.log(computerWin)
+    } else if (humanChoice < computerChoice) {
+        console.log(humanWin)
+    }    
 }
 getHumanChoice()
 getComputerChoice()
+playRound()
